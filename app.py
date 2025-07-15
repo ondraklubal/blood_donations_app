5b15ccfce5c4f5247e4a80e5c0a7141fc8ae4c10
Ondřej Klubal <109076299+ondraklubal@users.noreply.github.com>
2025-07-15 10:42:56 +0200

    Update app.py

--- app.py
+++ app.py
@@ -62,6 +62,7 @@ else:
                 st.success("Záznam uložen")
 
     # Statistiky
+    data_ws = pd.DataFrame(sheet.worksheet("data").get_all_records())
     user_data = data_df[data_df["username"] == st.session_state.username]
 
     user_data["date"] = pd.to_datetime(user_data["date"], errors='coerce')
